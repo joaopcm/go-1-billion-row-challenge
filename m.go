b2e6c571846b47839a74f8e2534f2541d6bbcf5f
João Melo <jopcmelo@gmail.com>
2024-08-15 10:38:22 -0300

    feat: open mock file and read it

--- m.go
+++ m.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+	"strings"
+)
 
+type Measurement struct {
+	Name  string
+	Min   float64
+	Max   float64
+	Sum   float64
+	Count int
+}
+
+func main() {
+	measurements, err := os.Open("measurements.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer measurements.Close()
+
+	scanner := bufio.NewScanner(measurements)
+	for scanner.Scan() {
+		rawData := scanner.Text()
+		semicolonIndex := strings.Index(rawData, ";")
+		location := rawData[:semicolonIndex]
+		temp := rawData[semicolonIndex+1:]
+	}
 }
